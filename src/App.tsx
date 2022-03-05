807158252b8bf6e2b9e80d6c58c8f21463056904
ts4blader <ts4blader@gmail.com>
2022-03-05 13:11:24 +0700

    remove config provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,19 @@ import "./styles/main.less";
 import Login from "pages/Login";
 import DashBoard from "pages/DashBoard";
 import { Switch, Route } from "react-router-dom";
-import { ConfigProvider } from "antd";
 
 function App() {
   return (
-    <ConfigProvider>
-      <div className="app">
-        <Switch>
-          <Route path="/" exact>
-            <Login />
-          </Route>
-          <Route path="/dashboard" exact>
-            <DashBoard />
-          </Route>
-        </Switch>
-      </div>
-    </ConfigProvider>
+    <div className="app">
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/dashboard" exact>
+          <DashBoard />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
