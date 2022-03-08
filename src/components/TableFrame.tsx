7f1fe9877414b1365d37ba8fe328a2d04c83856f
ts4blader <ts4blader@gmail.com>
2022-03-08 12:20:03 +0700

    add table frame

---
+++ src/components/TableFrame.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Table, Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+
+type Props = {
+  children: React.ReactNode;
+  title: string;
+  tableConfig: any;
+} & React.ComponentProps<"div">;
+
+const TableFrame = ({ children, title, tableConfig, ...rest }: Props) => {
+  return (
+    <div className="table-frame" {...rest}>
+      <div className="table-frame-header">{children}</div>
+      <div className="table-frame-body">
+        <div className="row">
+          <h3 className="title">{title}</h3>
+          <Input prefix={<SearchOutlined />} placeholder="Tìm kiếm" />
+        </div>
+        <Table
+          pagination={{
+            showSizeChanger: true,
+            pageSizeOptions: [8, 10, 15],
+            defaultPageSize: 8,
+          }}
+          rowKey={(record) => record.id}
+          {...tableConfig}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TableFrame;
