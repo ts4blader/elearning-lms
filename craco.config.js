a8026a4b19f320939823fb67eeedcef85b36da3a
ts4blader <ts4blader@gmail.com>
2022-03-08 16:40:16 +0700

    add craco alias

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const CracoLessPlugin = require("craco-less");
+const CracoAlias = require("craco-alias");
 
 module.exports = {
   plugins: [
@@ -11,6 +12,18 @@ module.exports = {
             javascriptEnabled: true,
           },
         },
+      },
+    },
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        // baseUrl SHOULD be specified
+        // plugin does not take it from tsconfig
+        baseUrl: "./src",
+        /* tsConfigPath should point to the file where "baseUrl" and "paths" 
+         are specified*/
+        tsConfigPath: "./tsconfig.paths.json",
       },
     },
   ],
