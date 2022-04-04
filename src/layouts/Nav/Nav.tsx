b67595a2f083c11a9fc67736ba443ee69e1fc234
ts4blader <ts4blader@gmail.com>
2022-04-04 19:21:37 +0700

    remove all unnesscessary constant

--- src/layouts/Nav/Nav.tsx
+++ src/layouts/Nav/Nav.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
-import { CATALOG, OPEN_KEYS } from "@constants/nav";
+import { CATALOG, OPEN_KEYS } from "./data";
 import { useHistory } from "react-router-dom";
 import Image from "@assets/Image";
 
