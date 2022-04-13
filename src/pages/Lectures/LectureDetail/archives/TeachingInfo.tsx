3f89b2729b5ca72c25e8339c70dd4b8b10f15d76
ts4blader <ts4blader@gmail.com>
2022-04-14 00:09:24 +0700

    teaching info data table

--- src/pages/Lectures/LectureDetail/archives/TeachingInfo.tsx
+++ src/pages/Lectures/LectureDetail/archives/TeachingInfo.tsx
@@ -1,5 +1,83 @@
 import React from "react";
+import { useAppDispatch } from "@hooks";
+import { showFormModal } from "@slices/formModalSlice";
+import { Button, Table } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import TableFrame from "@components/Table/TableFrame";
+import ItemActions from "@components/ItemActions";
+import teachingData from "@seeds/lecture/teaching-info.json";
+import { TEACHING_TABLE } from "./data";
+import { ColumnTitle } from "@components/Table";
+
+const AddButton = () => {
+  const dispatch = useAppDispatch();
+  const showModal = () => {
+    dispatch(
+      showFormModal({
+        title: "",
+        innerForm: null,
+      })
+    );
+  };
+
+  return (
+    <Button
+      type="primary"
+      size="large"
+      icon={<PlusOutlined />}
+      onClick={showModal}
+    >
+      Thêm
+    </Button>
+  );
+};
+
+const DataTable = () => {
+  const { Column } = Table;
+
+  return (
+    <Table
+      pagination={false}
+      rowKey={(record) => record.id}
+      dataSource={teachingData}
+      scroll={{
+        y: 300,
+      }}
+    >
+      {TEACHING_TABLE.map((item) =>
+        item.sorter ? (
+          <Column
+            {...item}
+            align="center"
+            title={({ sortColumns }) => (
+              <ColumnTitle
+                reactKey={item.key}
+                text={item.title}
+                sortColumns={sortColumns}
+                align="center"
+              />
+            )}
+          />
+        ) : (
+          <Column {...item} align="center" />
+        )
+      )}
+      <Column
+        key="actions"
+        align="center"
+        render={(text, record) => (
+          <ItemActions>
+            <ItemActions.EditButton title="" innerForm={() => null} />
+            <ItemActions.DeleteButton deleteName="" onDelete={() => null} />
+          </ItemActions>
+        )}
+      />
+    </Table>
+  );
+};
 
 export const TeachingInfo = () => {
-  return <div>TeachingInfo</div>;
+  return (
+    <TableFrame pageChanger={false} title={<AddButton />} table={DataTable} />
+  );
 };
--- src/pages/Lectures/LectureDetail/archives/data.ts
+++ src/pages/Lectures/LectureDetail/archives/data.ts
@@ -25,3 +25,37 @@ export const CAREER_TABLE = [
     key: "endDay",
   },
 ];
+
+export const TEACHING_TABLE = [
+  {
+    title: "Cơ quan/Đơn vị",
+    dataIndex: "unit",
+    key: "unit",
+    sorter: true,
+  },
+  {
+    title: "Chuyên ngành",
+    dataIndex: "major",
+    key: "major",
+  },
+  {
+    title: "Ngày bắt đầu",
+    dataIndex: "beginDay",
+    key: "beginDay",
+  },
+  {
+    title: "Ngày kết thúc",
+    dataIndex: "endDay",
+    key: "endDay",
+  },
+  {
+    title: "Văn bằng/Chứng chỉ",
+    dataIndex: "license",
+    key: "license",
+  },
+  {
+    title: "Hình thức",
+    dataIndex: "licenseType",
+    key: "licenseType",
+  },
+];
