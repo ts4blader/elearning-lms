f709aa424bafa7c1c98ceed6b2662ffa3b0259d6
ts4blader <ts4blader@gmail.com>
2022-05-12 15:38:16 +0700

    CRUD for score type

--- src/components/Forms/ScoreForm.tsx
+++ src/components/Forms/ScoreForm.tsx
@@ -4,33 +4,62 @@ import TextInput from "@components/TextInput";
 import { SelectInForm } from "@components/Select";
 import { RULES } from "@utils/rules";
 import { FormButton, FormItem } from "@components/Forms";
+import { FormModalGeneric, ScoreTypeProps } from "@types";
+import { useAppDispatch } from "@hooks";
+import { addScoreType, updateScoreType } from "@slices/scoreTypeSlice";
+import { generateId } from "@utils/methods";
 
-type Props = {
-  onCancel: () => void;
-};
-
-export const ScoreForm = ({ onCancel }: Props) => {
+export const ScoreForm = <T extends ScoreTypeProps>({
+  onCancel,
+  defaultData,
+}: FormModalGeneric<T>) => {
+  // submit handler
   const handleSubmit = (values: any) => {
-    console.log(values);
+    if (defaultData) {
+      dispatch(
+        updateScoreType({
+          ...values,
+          id: defaultData.id,
+        })
+      );
+    } else {
+      dispatch(
+        addScoreType({
+          ...values,
+          id: generateId("ld"),
+        })
+      );
+    }
+    form.resetFields();
   };
+  // form instance
+  const [form] = Form.useForm();
+  // redux hook
+  const dispatch = useAppDispatch();
 
   return (
-    <Form name="add-score" onFinish={handleSubmit} className="score-form">
+    <Form
+      name="add-score"
+      form={form}
+      initialValues={defaultData}
+      onFinish={handleSubmit}
+      className="score-form"
+    >
       <div className="form-top">
         <Space size={40}>
           <FormItem
             label="Tên loại điểm"
             className="name-field"
             name="name"
-            rules={[{ required: true, message: "Xin hãy nhập tên loại điểm" }]}
+            rules={[RULES.required]}
           >
             <TextInput />
           </FormItem>
           <FormItem
             label="Hệ số"
-            name="multiplier"
+            name="base"
             className="multiplier-field"
-            rules={[{ required: true, message: "Xin hãy chọn hệ số" }]}
+            rules={[RULES.required]}
           >
             <SelectInForm
               placeholder="Hệ số điểm"
@@ -49,20 +78,14 @@ export const ScoreForm = ({ onCancel }: Props) => {
           <FormItem
             name="first"
             label="Học kỳ 1"
-            rules={[
-              { required: true, message: "Xin hãy nhập điểm" },
-              RULES.number,
-            ]}
+            rules={[RULES.required, RULES.number]}
           >
             <TextInput />
           </FormItem>
           <FormItem
-            name="secondary"
+            name="second"
             label="Học kỳ 2"
-            rules={[
-              { required: true, message: "Xin hãy nhập điểm" },
-              RULES.number,
-            ]}
+            rules={[RULES.required, RULES.number]}
           >
             <TextInput />
           </FormItem>
--- src/pages/Database/tables/score/ScoreTable.tsx
+++ src/pages/Database/tables/score/ScoreTable.tsx
@@ -1,59 +1,76 @@
 import React from "react";
 import ItemActions from "@components/ItemActions";
-import DATA from "@seeds/thcs/scores.json";
 import { ScoreForm } from "@components/Forms";
 import { ColumnTitle, Table } from "@components/Table";
 import { Table as AntTable } from "antd";
+import { sortNumber, sortString } from "@utils/sortMethod";
+import { useAppDispatch, useAppSelector } from "@hooks";
+import { removeScoreType } from "@slices/scoreTypeSlice";
+import { ScoreTypeProps } from "@types";
 
 const ScoreTable = () => {
   const { Column } = Table;
   const { ColumnGroup } = AntTable;
 
+  // redux hook
+  const dispatch = useAppDispatch();
+  const scoreType = useAppSelector((state) => state.scoreType);
+
   return (
-    <Table rowKey={(record) => record.id} dataSource={DATA}>
+    <Table rowKey={(record) => record.id} dataSource={scoreType.value}>
       <Column
         title={({ sortColumns }) => (
-          <ColumnTitle sortColumns={sortColumns} text="Name" reactKey="name" />
+          <ColumnTitle
+            sortColumns={sortColumns}
+            text="Loại điểm"
+            reactKey="name"
+            align="center"
+          />
         )}
-        dataIndex="name"
+        render={(text, record: ScoreTypeProps) => `Kiểm tra ${record.name}`}
+        align="center"
         key="name"
-        sorter={true}
+        sorter={(a, b) => sortString(a.name, b.name)}
       />
       <Column
         title={({ sortColumns }) => (
-          <ColumnTitle sortColumns={sortColumns} text="Base" reactKey="base" />
+          <ColumnTitle
+            sortColumns={sortColumns}
+            align="center"
+            text="Hệ số"
+            reactKey="base"
+          />
         )}
         dataIndex="base"
         key="base"
-        sorter={true}
+        align="center"
+        sorter={(a, b) => sortNumber(a.base, b.base)}
       />
 
-      <ColumnGroup title="Minimum scores amount" align="center">
+      <ColumnGroup title="Số cột điểm tối thiểu" align="center">
+        <Column title="Học kỳ 1" dataIndex="first" key="first" align="center" />
         <Column
-          title="1st semester"
-          dataIndex="first"
-          key="first"
-          align="center"
-        />
-        <Column
-          title="2nd semester"
-          dataIndex="secondary"
-          key="secondary"
+          title="Học kỳ 2"
+          dataIndex="second"
+          key="second"
           align="center"
         />
       </ColumnGroup>
       {/* Item actions */}
       <Column
+        align="center"
         key="action"
-        render={(text, record) => (
+        render={(text, record: ScoreTypeProps) => (
           <ItemActions>
             <ItemActions.EditButton
               title="Thiết lập loại điểm"
-              innerForm={ScoreForm}
+              innerForm={(props) => (
+                <ScoreForm {...props} defaultData={record} />
+              )}
             />
             <ItemActions.DeleteButton
               deleteName="loại điểm"
-              onDelete={() => null}
+              onDelete={() => dispatch(removeScoreType(record.id))}
             />
           </ItemActions>
         )}
